c34da67a1494e8836522fa6547b93602721da7ca
daihaorui <Dai_Haorui@163.com>
2024-10-17 17:19:17 +0800

    移除自定义用户,增加allauth配置

--- mydemo/apps/account/apps.py
+++ mydemo/apps/account/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
+    label = 'account_config'  # 这里指定一个唯一的 app_label
